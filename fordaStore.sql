fded9e41581447e286ee1f2868613576ade6eca3
Yoshifumi12 <yoshifumisuzuki81194@gmail.com>
2022-12-05 00:13:54 +0800

    scope: database update
    Description: removed tables permission and hasPermission
    
    Expected behavior:
    create a database with 4 tables
    Tags:
    
    see: Issue #

--- fordaStore.sql
+++ fordaStore.sql
@@ -1,37 +1,31 @@
 DROP DATABASE IF EXISTS fordaStore;
 CREATE DATABASE fordaStore;
 USE fordaStore;
-
-CREATE TABLE User(
-  userID INT AUTO_INCREMENT,
+CREATE TABLE User (
+  userID INT AUTO_INCREMENT NOT NULL,
   username VARCHAR(255) NOT NULL,
   userPassword VARCHAR(255) NOT NULL,
   PRIMARY KEY(userID)
 );
-CREATE TABLE Permission(
-  permID INT AUTO_INCREMENT,
-  description VARCHAR(255),
-  PRIMARY KEY(permID)
-);
-CREATE TABLE HasPermission(
-  hasPermissionID INT AUTO_INCREMENT,
+CREATE TABLE Container (
+  containerID INT AUTO_INCREMENT NOT NULL,
+  containerTitle VARCHAR(255) NOT NULL,
   userID INT,
-  permiID INT,
-  containerID INT,
-  PRIMARY KEY(hasPermissionID),
-  FOREIGN KEY(userID) REFERENCES User(userID),
-  FOREIGN KEY(permiID) REFERENCES Permission(permID),
-  FOREIGN KEY(containerID) REFERENCES Container(containerID)
-);
-CREATE TABLE Container(
-  containerID INT AUTO_INCREMENT,
-  containerTitle VARCHAR(255),
   PRIMARY KEY(containerID),
+  FOREIGN KEY(userID) REFERENCES User(userID)
 );
-CREATE TABLE Content(
-  contentID INT AUTO_INCREMENT,
-  contentText VARCHAR(255),
-  containerID INT,
+CREATE TABLE ContentCredentials(
+  contentCredentialsID INT AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY(contentCredentialsID)
+);
+CREATE TABLE Content (
+  contentID INT AUTO_INCREMENT NOT NULL,
+  contentText VARCHAR(255) NOT NULL,
+  containerID INT NOT NULL,
+  contentCredentialsID INT,
   PRIMARY KEY(contentID),
-  FOREIGN KEY(containerID) REFERENCES Container(containerID)
+  FOREIGN KEY(containerID) REFERENCES Container(containerID),
+  FOREIGN KEY(contentCredentialsID) REFERENCES ContentCredentials(contentCredentialsID)
 );
