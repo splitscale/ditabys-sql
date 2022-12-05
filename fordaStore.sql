28c1f0257b9b9b31ecf5bfdc5883865bb80f4273
Yoshifumi12 <yoshifumisuzuki81194@gmail.com>
2022-12-05 16:35:52 +0800

    scope: database update
    Description: changed userPassword to password in table User
    
    Expected behavior:
    create a database and tables
    Tags:
    
    see: Issue #

--- fordaStore.sql
+++ fordaStore.sql
@@ -3,29 +3,29 @@ CREATE DATABASE fordaStore;
 USE fordaStore;
 
 CREATE TABLE User (
-  userID INT AUTO_INCREMENT NOT NULL,
+  userID BIGINT AUTO_INCREMENT NOT NULL,
   username VARCHAR(255) NOT NULL,
-  userPassword VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   PRIMARY KEY(userID)
 );
 CREATE TABLE Container (
-  containerID INT AUTO_INCREMENT NOT NULL,
+  containerID BIGINT AUTO_INCREMENT NOT NULL,
   containerTitle VARCHAR(255) NOT NULL,
-  userID INT,
+  userID BIGINT,
   PRIMARY KEY(containerID),
   FOREIGN KEY(userID) REFERENCES User(userID)
 );
 CREATE TABLE ContentCredentials(
-  contentCredentialsID INT AUTO_INCREMENT,
+  contentCredentialsID BIGINT AUTO_INCREMENT,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   PRIMARY KEY(contentCredentialsID)
 );
 CREATE TABLE Content (
-  contentID INT AUTO_INCREMENT NOT NULL,
+  contentID BIGINT AUTO_INCREMENT NOT NULL,
   contentText VARCHAR(255) NOT NULL,
-  containerID INT NOT NULL,
-  contentCredentialsID INT,
+  containerID BIGINT NOT NULL,
+  contentCredentialsID BIGINT,
   PRIMARY KEY(contentID),
   FOREIGN KEY(containerID) REFERENCES Container(containerID),
   FOREIGN KEY(contentCredentialsID) REFERENCES ContentCredentials( contentCredentialsID)
