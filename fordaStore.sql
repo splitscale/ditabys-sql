2df7052985cb91f8d63be2c14acf4790e36cac96
kasutu <jeromecabugwason2@protonmail.com>
2022-12-11 14:17:11 +0800

    db: Updated script
    Description:
    updated script to only build and
    used standard naming strategy
    
    added a new database `sessions` for
    JWT tokens
    
    Expected behavior:
    able to store JWT and and use standard
    naming strategy

--- fordaStore.sql
+++ fordaStore.sql
@@ -1,47 +1,44 @@
-DROP DATABASE IF EXISTS fordaStore;
-CREATE DATABASE fordaStore;
-USE fordaStore;
+DROP DATABASE IF EXISTS sessions;
+CREATE DATABASE sessions;
+USE sessions;
 
-CREATE TABLE User (
-  userID BIGINT AUTO_INCREMENT NOT NULL,
+CREATE TABLE token (
+  token_id BIGINT AUTO_INCREMENT NOT NULL,
+  uid VARCHAR(255) UNIQUE NOT NULL,
+  public_key VARCHAR(1024) UNIQUE NOT NULL,
+  PRIMARY KEY(token_id)
+);
+
+DROP DATABASE IF EXISTS fordastore;
+CREATE DATABASE fordastore;
+USE fordastore;
+
+CREATE TABLE user (
+  user_id BIGINT AUTO_INCREMENT NOT NULL,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  uid VARCHAR(255) NOT NULL UNIQUE,
+  PRIMARY KEY(user_id)
+);
+CREATE TABLE container (
+  container_id BIGINT AUTO_INCREMENT NOT NULL,
+  container_title VARCHAR(255) NOT NULL,
+  user_id BIGINT NOT NULL,
+  PRIMARY KEY(container_id),
+  FOREIGN KEY(user_id) REFERENCES user(user_id)
+);
+CREATE TABLE url_credentials (
+  credentials_id BIGINT AUTO_INCREMENT,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  PRIMARY KEY(userID)
+  PRIMARY KEY(credentials_id)
 );
-CREATE TABLE Container (
-  containerID BIGINT AUTO_INCREMENT NOT NULL,
-  containerTitle VARCHAR(255) NOT NULL,
-  userID BIGINT,
-  PRIMARY KEY(containerID),
-  FOREIGN KEY(userID) REFERENCES User(userID)
+CREATE TABLE url (
+  url_id BIGINT AUTO_INCREMENT NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  container_id BIGINT NOT NULL,
+  credentials_id BIGINT,
+  PRIMARY KEY(url_id),
+  FOREIGN KEY(container_id) REFERENCES container(container_id),
+  FOREIGN KEY(credentials_id) REFERENCES url_credentials(credentials_id)
 );
-CREATE TABLE ContentCredentials(
-  contentCredentialsID BIGINT AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  PRIMARY KEY(contentCredentialsID)
-);
-CREATE TABLE Content (
-  contentID BIGINT AUTO_INCREMENT NOT NULL,
-  contentText VARCHAR(255) NOT NULL,
-  containerID BIGINT NOT NULL,
-  contentCredentialsID BIGINT,
-  PRIMARY KEY(contentID),
-  FOREIGN KEY(containerID) REFERENCES Container(containerID),
-  FOREIGN KEY(contentCredentialsID) REFERENCES ContentCredentials( contentCredentialsID)
-);
-DELIMITER && 
-CREATE PROCEDURE getUserContainers(IN userID BIGINT)
-BEGIN 
-SELECT * FROM Container 
-WHERE Container.userID = userID;
-END&&
-DELIMITER ;  
-
-DELIMITER && 
-CREATE PROCEDURE getContainerContent(IN containerID BIGINT)
-BEGIN 
-SELECT * FROM Content 
-WHERE Content.contentID = containerID;
-END&&
-DELIMITER ;  
