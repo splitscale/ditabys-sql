2bf0225d0677079d2cdfc60b77ace1eccd964dd6
Yoshifumi12 <yoshifumisuzuki81194@gmail.com>
2022-12-21 11:13:53 +0800

    scope:
    Description:
    
    Expected behavior:
    
    Tags:
    
    see: Issue #

---
+++ auth.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS auth;
+CREATE DATABASE auth;
+USE auth;
+
+CREATE TABLE token (
+  token_id BIGINT NOT NULL,
+  uid VARCHAR(255) UNIQUE NOT NULL,
+  public_key VARCHAR(3000) NOT NULL,
+  PRIMARY KEY(token_id)
+);
