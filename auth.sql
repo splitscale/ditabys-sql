eab53db629b485cb129e047ce432314a585ddb5c
Yoshifumi12 <yoshifumisuzuki81194@gmail.com>
2022-12-22 16:41:05 +0800

    scope: update
    Description: added procedures to get, set, and update values to tables
    
    Expected behavior: create db, tables, and procedures
    
    Tags:
    
    see: Issue #

--- auth.sql
+++ auth.sql
@@ -3,8 +3,34 @@ CREATE DATABASE auth;
 USE auth;
 
 CREATE TABLE token (
-  token_id BIGINT NOT NULL,
+  token_id BIGINT AUTO_INCREMENT NOT NULL,
   uid VARCHAR(255) UNIQUE NOT NULL,
   public_key VARCHAR(3000) NOT NULL,
   PRIMARY KEY(token_id)
 );
+
+DELIMITER && 
+CREATE PROCEDURE newToken(IN uid BIGINT, public_key VARCHAR(3000))
+BEGIN 
+INSERT INTO token (uid, public_key)
+VALUES (uid, public_key)
+END&&
+DELIMITER ;
+
+DELIMITER && 
+CREATE PROCEDURE updateToken(IN token_id BIGINT, uid VARCHAR(255), public_key VARCHAR(3000) )
+BEGIN 
+UPDATE token
+SET token_id = token_id, uid= uid, public_key = public_key
+WHERE token_id = token_id;
+END&&
+DELIMITER ;
+
+DELIMITER && 
+CREATE PROCEDURE deleteToken(IN token_id BIGINT)
+BEGIN 
+DELETE FROM token
+WHERE token_id = token_id;
+END&&
+DELIMITER ;
+
