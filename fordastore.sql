5d97033f7cb5dd7e67638fc3be20776676c8c86b
Jerome Cabugwason <jeromecabugwason2@protonmail.com>
2023-01-10 20:25:16 +0800

    Update fordastore.sql

--- fordastore.sql
+++ fordastore.sql
@@ -1,33 +1,25 @@
-DROP DATABASE IF EXISTS fordastore;
-CREATE DATABASE fordastore;
-USE fordastore;
+DROP DATABASE IF EXISTS dev;
+CREATE DATABASE dev;
+USE dev;
 
 CREATE TABLE user (
-  user_id BIGINT AUTO_INCREMENT NOT NULL,
-  username VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
-  uid VARCHAR(255) NOT NULL UNIQUE,
-  PRIMARY KEY(user_id)
+	user_id binary(16) PRIMARY KEY NOT NULL,
+	username VARCHAR(50) NOT NULL UNIQUE,
+	password VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE container (
   container_id BIGINT AUTO_INCREMENT NOT NULL,
   container_title VARCHAR(255) NOT NULL,
-  user_id BIGINT NOT NULL,
+  user_id binary(16) NOT NULL,
   PRIMARY KEY(container_id),
   FOREIGN KEY(user_id) REFERENCES user(user_id)
 );
-CREATE TABLE url_credentials (
-  credentials_id BIGINT AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  PRIMARY KEY(credentials_id)
-);
+
 CREATE TABLE url (
   url_id BIGINT AUTO_INCREMENT NOT NULL,
   url VARCHAR(2000) NOT NULL,
   container_id BIGINT NOT NULL,
-  credentials_id BIGINT,
   PRIMARY KEY(url_id),
-  FOREIGN KEY(container_id) REFERENCES container(container_id),
-  FOREIGN KEY(credentials_id) REFERENCES url_credentials(credentials_id)
+  FOREIGN KEY(container_id) REFERENCES container(container_id)
 );
